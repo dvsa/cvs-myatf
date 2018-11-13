d09244de532825e8f74448409389309651c3c69a
David Yates <davidyates@deloitte.co.uk>
2018-11-13 23:36:00 +0000

    Initial redesign for sprint 6

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Prototype: Manage your ATF (MyATF)',
+  serviceName: 'Prototype: Manage your ATF',
 
   // Default port that prototype runs on
   port: '3000',
