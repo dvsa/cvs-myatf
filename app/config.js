e3bfc2fe6c859a45e148e9dfa361327c994a785a
Zahraa Akhtar <zahakhtar@UK-C02Z711ULVDM>
2020-04-29 16:42:09 +0100

    update to header to say MYATF, added change and reset password pages, updated mfa and create account errors

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage your Authorised Testing Facility',
+  serviceName: 'Manage Your Authorised Testing Facility',
 
   // Default port that prototype runs on
   port: '3000',
